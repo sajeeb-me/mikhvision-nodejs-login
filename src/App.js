4abc929c9f00918c23da41a59542ad2169bed354
sdshuvo98 <hellosdshuvo@gmail.com>
2022-08-03 17:41:22 +0600

    developed routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from Node.js Login</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
